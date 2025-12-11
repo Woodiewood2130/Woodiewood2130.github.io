ed52bff619144232db7fa90aa58c9d15b7f641c3
Woodiewood_2130 <mohddawood2002@gmail.com>
2025-12-11 15:02:50 +0300

    v1.3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Woodiewood2130.github.io/',
+    base: '/', // Root path for user GitHub Pages (woodiewood2130.github.io)
 })
